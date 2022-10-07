2499b58744db0c3bc118f8795ef964d6a398f3a0
HM <hieumdd@gmail.com>
2022-10-07 17:12:02 +0700

    feat: Add OG

--- pages/mega-sales-10-10.tsx
+++ pages/mega-sales-10-10.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { NextSeo } from 'next-seo';
 import {
     Container,
     HStack,
@@ -52,77 +53,100 @@ const Countdown = ({ title }: CountdownProps) => (
 );
 
 const MegaSales1010 = () => (
-    <Container maxW="md" backgroundColor="white" pb="32px">
-        <VStack alignItems="stretch" spacing="1rem">
-            <ChakraNextImage
-                src="/mega-sales-10-10/product/banner-1.png"
-                alt="Mega Sales 10-10"
-            />
+    <>
+        <NextSeo
+            openGraph={{
+                type: 'website',
+                url: 'https://khuyenmai2.vuanem.com/mega-sales-10-10',
+                title: 'Mega Sales 10-10 - Vua Nệm',
+                description: 'Mega Sales 10-10 với ngàn ưu đãi',
+                images: [
+                    {
+                        url: '/mega-sales-10-10/product/banner-1.png',
+                        width: 500,
+                        height: 500,
+                        alt: 'Banner',
+                    },
+                ],
+            }}
+            twitter={{
+                handle: '@avneesh0612',
+                site: '@avneesh0612',
+                cardType: 'summary_large_image',
+            }}
+        />
+        <Container maxW="md" backgroundColor="white" pb="32px">
             <VStack alignItems="stretch" spacing="1rem">
-                <DealHot />
-                <SimpleGrid columns={2} spacing="1rem">
-                    <Product
-                        src="/mega-sales-10-10/product/comodo-1.png"
-                        alt="Comodo"
-                        href="https://vuanem.com/nem-foam-amando-comodo-luxury.html"
-                    />
-                    <Product
-                        src="/mega-sales-10-10/product/evita-1.png"
-                        alt="Evita"
-                        href="https://vuanem.com/nem-lo-xo-dunlopillo-evita.html"
-                    />
-                    <Product
-                        src="/mega-sales-10-10/product/gummi-legend-1.png"
-                        alt="Gummi Legend"
-                        href="https://vuanem.com/nem-cao-su-gummi-legend.html"
-                    />
-                    <Product
-                        src="/mega-sales-10-10/product/lamore-1.png"
-                        alt="L'Amore"
-                        href="https://vuanem.com/nem-foam-amando-l-amore.html"
-                    />
-                </SimpleGrid>
-            </VStack>
-            <VStack alignItems="stretch" spacing="1rem">
-                <Countdown title="Mega Sale Shock" />
-                <Product
-                    src="/mega-sales-10-10/product/faro-1.png"
-                    alt="Faro"
-                    href="https://vuanem.com/nem-lo-xo-amando-faro.html"
+                <ChakraNextImage
+                    src="/mega-sales-10-10/product/banner-1.png"
+                    alt="Mega Sales 10-10"
                 />
+                <VStack alignItems="stretch" spacing="1rem">
+                    <DealHot />
+                    <SimpleGrid columns={2} spacing="1rem">
+                        <Product
+                            src="/mega-sales-10-10/product/comodo-1.png"
+                            alt="Comodo"
+                            href="https://vuanem.com/nem-foam-amando-comodo-luxury.html"
+                        />
+                        <Product
+                            src="/mega-sales-10-10/product/evita-1.png"
+                            alt="Evita"
+                            href="https://vuanem.com/nem-lo-xo-dunlopillo-evita.html"
+                        />
+                        <Product
+                            src="/mega-sales-10-10/product/gummi-legend-1.png"
+                            alt="Gummi Legend"
+                            href="https://vuanem.com/nem-cao-su-gummi-legend.html"
+                        />
+                        <Product
+                            src="/mega-sales-10-10/product/lamore-1.png"
+                            alt="L'Amore"
+                            href="https://vuanem.com/nem-foam-amando-l-amore.html"
+                        />
+                    </SimpleGrid>
+                </VStack>
+                <VStack alignItems="stretch" spacing="1rem">
+                    <Countdown title="Mega Sale Shock" />
+                    <Product
+                        src="/mega-sales-10-10/product/faro-1.png"
+                        alt="Faro"
+                        href="https://vuanem.com/nem-lo-xo-amando-faro.html"
+                    />
+                </VStack>
+                <VStack alignItems="stretch" spacing="1rem">
+                    <Countdown title="Nệm ấm giá êm" />
+                    <SimpleGrid columns={2} spacing="1rem">
+                        <Product
+                            src="/mega-sales-10-10/product/gummi-latex-1.png"
+                            alt="Gummi Latex"
+                            href="https://vuanem.com/nem-cao-su-gummi-latex-7zones.html"
+                        />
+                        <Product
+                            src="/mega-sales-10-10/product/luca-1.png"
+                            alt="Luca"
+                            href="https://vuanem.com/nem-foam-amando-luca.html"
+                        />
+                    </SimpleGrid>
+                </VStack>
+                <VStack alignItems="stretch" spacing="1rem">
+                    <Countdown title="Siêu Sale Phụ Kiện" />
+                    <SimpleGrid columns={2} spacing="1rem">
+                        <Product
+                            src="/mega-sales-10-10/product/flora-1.png"
+                            alt="Flora"
+                            href="https://vuanem.com/chan-chan-amando-flora.html"
+                        />
+                        <Product
+                            src="/mega-sales-10-10/product/doona-bamboo-1.png"
+                            alt="Comodo"
+                            href="https://vuanem.com/bao-ve-nem-doona-bamboo-chong-tham.html"
+                        />
+                    </SimpleGrid>
+                </VStack>
             </VStack>
-            <VStack alignItems="stretch" spacing="1rem">
-                <Countdown title="Nệm ấm giá êm" />
-                <SimpleGrid columns={2} spacing="1rem">
-                    <Product
-                        src="/mega-sales-10-10/product/gummi-latex-1.png"
-                        alt="Gummi Latex"
-                        href="https://vuanem.com/nem-cao-su-gummi-latex-7zones.html"
-                    />
-                    <Product
-                        src="/mega-sales-10-10/product/luca-1.png"
-                        alt="Luca"
-                        href="https://vuanem.com/nem-foam-amando-luca.html"
-                    />
-                </SimpleGrid>
-            </VStack>
-            <VStack alignItems="stretch" spacing="1rem">
-                <Countdown title="Siêu Sale Phụ Kiện" />
-                <SimpleGrid columns={2} spacing="1rem">
-                    <Product
-                        src="/mega-sales-10-10/product/flora-1.png"
-                        alt="Flora"
-                        href="https://vuanem.com/chan-chan-amando-flora.html"
-                    />
-                    <Product
-                        src="/mega-sales-10-10/product/doona-bamboo-1.png"
-                        alt="Comodo"
-                        href="https://vuanem.com/bao-ve-nem-doona-bamboo-chong-tham.html"
-                    />
-                </SimpleGrid>
-            </VStack>
-        </VStack>
-    </Container>
+        </Container>
+    </>
 );
 
 export const getStaticProps = async () => ({
